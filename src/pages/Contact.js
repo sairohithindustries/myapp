b528ad8e2733a64e79028f887bd562e05ae5d470
Praneeth Karnatakam <98795889+Biryani-Times@users.noreply.github.com>
2025-04-26 23:47:55 +0530

    font of phone number and email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -50,10 +50,9 @@ function Contact() {
   return (
     <>
     <Font family='Montserrat' weight={1000} italic={true}>
-     <div style = {{ textAlign: "center"}}>
+    <div style = {{ textAlign: "center"}}>
     <h1>CONTACT</h1>
-    </div> 
-    </Font>
+    </div>   
     <div style={styles.container}>
     <div style={styles.contactItem}>
       <Call02Icon size={20} fill="black"></Call02Icon> <a href="tel:08552226594">08552226594</a>
@@ -71,9 +70,7 @@ function Contact() {
 
 
 
-    {/* Google Maps Embed */}
     <div style={styles.mapContainer}>
-     <div>Google Maps Link</div>
      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3850.9651741969387!2d77.4155130104954!3d15.160257163069364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb6ef6de23f4813%3A0x99bca59748d410e8!2sSai%20Rohith%20Industries!5e0!3m2!1sen!2sin!4v1742580711669!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
       <iframe
         style={styles.map}
@@ -84,6 +81,7 @@ function Contact() {
       ></iframe>
     </div>
   </div>
+   </Font>
   </>
   )
 }
